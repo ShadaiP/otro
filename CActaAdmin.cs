fa2acaef84c01dcebfc1f5f1c6ff34b66f6e2fe0
ShadaiP <75696719+ShadaiP@users.noreply.github.com>
2020-12-17 23:34:54 -0600

    Carpeta CNegocios
    Es el envio de infromacion entre las paginas

---
+++ CActaAdmin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InventariosPJEH.CNegocios
+{
+    public class CActaAdmin
+    {
+        public long TxtFiltroB { get; set; }
+
+        public string TxtNombreRes { get; set; }
+
+        public string TxTCatgoR { get; set; }
+
+        public string UTxtAreaAdri { get; set; }
+    }
+}
